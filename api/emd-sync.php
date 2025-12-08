07306d59631fa32f3dca3b290bbd7215474233b6
hypax <github@hypax.wtf>
2025-12-08 17:26:50 +0100

    Sets default timezone to Europe/Berlin
    Ensures consistent date and time handling across the application by setting the default timezone to Europe/Berlin.

--- api/emd-sync.php
+++ api/emd-sync.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
+date_default_timezone_set('Europe/Berlin');
+
 session_start();
 require_once __DIR__ . '/../assets/config/config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
--- assets/functions/enotf/enrbridge.php
+++ assets/functions/enotf/enrbridge.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set('Europe/Berlin');
+
 session_start();
 require_once __DIR__ . '/../../../assets/config/config.php';
 require __DIR__ . '/../../../assets/config/database.php';
